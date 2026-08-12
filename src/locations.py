ad0db8b1e243c5712794cf6c2978754724e7371c
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-08-12 13:16:59 -0400

    updated naming

--- src/locations.py
+++ src/locations.py
@@ -1,4 +1,4 @@
-
+"""Handles logic for distance calculations"""
 
 from pathlib import Path
 
@@ -11,12 +11,16 @@ PROCESSED_DIR = Path("data/processed")
 PROCESSED_PATH = PROCESSED_DIR / "dnr_up_hiking_trails_grouped.parquet"
 
 MICHIGAN_GEOREF = "EPSG:3078"
+METERS_PER_MILE = 1609.344
 
 trails = gpd.read_parquet(PROCESSED_PATH)
+trails_projected = trails.to_crs(MICHIGAN_GEOREF)
+
+
+
+user_zipcode = "49781" #St. Ignace
 
 zip_lookup = pgeocode.Nominatim("us")
-
-user_zipcode = "49781"
 
 location = zip_lookup.query_postal_code(user_zipcode)
 
@@ -32,5 +36,16 @@ user_location = gpd.GeoDataFrame(
 )
 user_location_projected = user_location.to_crs(MICHIGAN_GEOREF)
 
-trails_projected = trails.to_crs(MICHIGAN_GEOREF)
+user_point_projected = user_location_projected.geometry.iloc[0]
+
+trails_projected["DistanceMiles"] = (
+    trails_projected.geometry.distance(user_point_projected)
+    / METERS_PER_MILE
+)
+
+print(
+    trails_projected
+    .sort_values("DistanceMiles")
+    .head(5)
+)
 
